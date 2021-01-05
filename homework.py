3348351cb937334c691918292ac68d7eae3947cb
Arsenii <belyakov.arseny2014@gmail.com>
2021-01-05 19:50:02 +0300

    Review check

--- homework.py
+++ homework.py
@@ -9,11 +9,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class APIError(Exception):
-    def __init__(self, text):
-        self.txt = text
-
-
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -21,7 +16,7 @@ YANDEX_HOMEWORK_URL = ('https://praktikum.yandex.ru/'
                        'api/user_api/homework_statuses/')
 REQUEST_HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 HOMEWORK_IS_CHECKED = 'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-HOMEWORK_STATUS = {
+HOMEWORK_STATUSES = {
     'rejected': 'К сожалению в работе нашлись ошибки.',
     'approved': 'Ревьюеру всё понравилось, '
                 'можно приступать к следующему уроку.',
@@ -34,57 +29,59 @@ LOG_VALUE_ERROR = ('Json doesnt contain expected homework status values. '
                    'Json value: {}')
 LOG_CONNECTION_ERROR = 'Request faced an error: {error}!\n' + REQUEST_LOG
 LOG_API_ERROR = 'Server said that he faced a trouble: {error}!\n' + REQUEST_LOG
+LOG_SENT_MESSAGE = 'Have sent a message: {}'
+
+bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
+    status = homework['status']
     # Check if json contains expected values
-    if homework['status'] not in HOMEWORK_STATUS:
+    if status not in HOMEWORK_STATUSES:
         raise ValueError(
-           LOG_VALUE_ERROR.format(homework['status'])
+           LOG_VALUE_ERROR.format(status)
         )
-    verdict = HOMEWORK_STATUS[homework['status']]
     return HOMEWORK_IS_CHECKED.format(
         homework_name=homework['homework_name'],
-        verdict=verdict
+        verdict=HOMEWORK_STATUSES[status]
     )
 
 
 def get_homework_statuses(current_timestamp):
-    headers = REQUEST_HEADERS
     params = {'from_date': current_timestamp}
+    request_log = dict(
+        url=YANDEX_HOMEWORK_URL,
+        headers=REQUEST_HEADERS,
+        params=params
+    )
     try:
         response = requests.get(
             YANDEX_HOMEWORK_URL,
-            headers=headers,
+            headers=REQUEST_HEADERS,
             params=params
         )
     except requests.RequestException as error:
         raise ConnectionError(
             LOG_CONNECTION_ERROR.format(
                 error=error,
-                url=YANDEX_HOMEWORK_URL,
-                headers=headers,
-                params=params
+                **request_log
             )
         )
     response_json = response.json()
     # Check if server sent an error message
-    server_error_keys = ['error', 'code']
-    for error_key in server_error_keys:
+    for error_key in ['error', 'code']:
         if error_key in response_json:
-            raise APIError(
+            raise ValueError(
                 LOG_API_ERROR.format(
                     error=response_json[error_key],
-                    url=YANDEX_HOMEWORK_URL,
-                    headers=headers,
-                    params=params
+                    **request_log
                 )
             )
     return response_json
 
 
 def send_message(message, bot_client):
-    logging.info(f'Have sent a message: {message}')
+    logging.info(LOG_SENT_MESSAGE.format(message))
     return bot_client.send_message(CHAT_ID, message)
 
 
@@ -93,11 +90,11 @@ def main():
     logging.debug('Bot has launched')
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
+            new_homework = get_homework_statuses(0)
             if new_homework.get('homeworks'):
                 send_message(
                     parse_homework_status(new_homework.get('homeworks')[0]),
-                    bot_client=telegram.Bot(token=TELEGRAM_TOKEN)
+                    bot_client
                 )
             current_timestamp = new_homework.get(
                 'current_date',
@@ -113,7 +110,6 @@ def main():
 
 
 if __name__ == '__main__':
-    bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
     logging.basicConfig(
         level=logging.DEBUG,
         filename=__file__ + '.log',
